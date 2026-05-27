98dff253f8c511b442ec09181950fa2cea970c35
dome323 <domeuyunkar@gmail.com>
2026-05-26 21:42:04 -0500

    feat: agregar controles de navegacion de paginas en simpsons-page

--- src/app/features/simpsons/pages/simpsons-page/simpsons-page.ts
+++ src/app/features/simpsons/pages/simpsons-page/simpsons-page.ts
@@ -1,27 +1,35 @@
-import { Component, inject } from '@angular/core';
-
+import { Component, inject, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
-
 import { rxResource } from '@angular/core/rxjs-interop';
 
 import { SimpsonsService } from '../../services/simpsons.service';
+import { PaginationService } from '../../../../shared/services/pagination.service';
 
 @Component({
   selector: 'app-simpsons-page',
-
   standalone: true,
-
   imports: [RouterLink],
-
   templateUrl: './simpsons-page.html',
 })
 
 export class SimpsonsPageComponent {
-
   private simpsonsService = inject(SimpsonsService);
+  paginationService = inject(PaginationService);
+  readonly charactersPerPage = signal(10);
 
   simpsonsResource = rxResource({
-    stream: () => this.simpsonsService.getCharacters(1),
+
+    params: () => ({
+      page: this.paginationService.currentPage(),
+      limit: this.charactersPerPage(),
+    }),
+
+    stream: ({ params }) =>
+      this.simpsonsService.getCharactersOptions({
+        page: params.page,
+        limit: params.limit,
+      }),
+
   });
 
 }
